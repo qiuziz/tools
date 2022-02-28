e314841dcfc076b15eb9a39957def5cb52f1bd94
Qiuz <zhu_anly@163.com>
2022-02-28 10:28:19 +0800

    Update constant.ts

--- src/pages/site-map/constant.ts
+++ src/pages/site-map/constant.ts
@@ -29,5 +29,81 @@ export const LIST = [
   {
     title: 'PDF转换器',
     url: 'https://www.pdf2go.com/zh'
+  },
+  {
+    title: '正则图示化',
+    url: 'https://jex.im/regulex/#!flags=&re=%5E(a%7Cb)*%3F%24'
+  },
+  {
+    title: '代码压缩',
+    url: 'https://tool.css-js.com/'
+  },
+  {
+    title: '代码格式化',
+    url: 'https://tool.oschina.net/codeformat/js/'
+  },
+  {
+    title: 'MP4转GIF',
+    url: 'https://www.aconvert.com/cn/video/mp4-to-gif/'
+  },
+  {
+    title: '图标工厂-一键生成所有尺寸的应用图标',
+    url: 'https://icon.wuruihong.com/#/custom'
+  },
+  {
+    title: 'SVG编辑器',
+    url: 'http://www.zuohaotu.com/svg/'
+  },
+  {
+    title: 'word文档翻译',
+    url: 'https://www.onlinedoctranslator.com/zh-CN/'
+  },
+  {
+    title: 'PDF转WORD 以及更多',
+    url: 'https://www.alltoall.net/'
+  },
+  {
+    title: '在线DES加密解密',
+    url: 'http://tool.chacuo.net/cryptdes'
+  },
+  {
+    title: 'Ping检测',
+    url: 'http://mping.chinaz.com/?host=3.114.171.17'
+  },
+  {
+    title: '端口扫描',
+    url: 'http://tool.chinaz.com/port/'
+  },
+  {
+    title: 'Mac精品软件-macwk',
+    url: 'https://macwk.com/'
+  },
+  {
+    title: 'Mac精品软件-digit77',
+    url: 'https://www.digit77.com/'
+  },
+  {
+    title: 'loading图标生成-loading.io',
+    url: 'https://loading.io/'
+  },
+  {
+    title: 'less编译',
+    url: 'https://1024tools.com/less'
+  },
+  {
+    title: '阿里图标-iconfont',
+    url: 'https://www.iconfont.cn/'
+  },
+  {
+    title: '字体生成转换',
+    url: 'https://transfonter.org/'
+  },
+  {
+    title: 'Chrome inspect',
+    url: 'chrome://inspect/#devices'
+  },
+  {
+    title: '字体生成转换',
+    url: 'https://transfonter.org/'
   }
 ];
